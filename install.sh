e973ffb6ecff1bd743d94b4e9c23c70f01ba64f7
m-shigemori <yasumasashige790@gmail.com>
2025-03-30 15:14:14 +0900

    [FIX] launch file

--- install.sh
+++ install.sh
@@ -7,10 +7,4 @@ pip3 install ultralytics
 pip3 install super-gradients
 pip3 install lap
 
-
-cd ~/colcon_ws/src/
-git clone -b feature/humble-devel https://github.com/TeamSOBITS/bbox_to_tf.git
-cd bbox_to_tf/
-bash install.sh
-
 echo "╚══╣ Install: YOLO ROS (FINISHED) ╠══╝"
