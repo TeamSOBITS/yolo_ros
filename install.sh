addff9898d338630493c99037c7fe0a39b5542ed
m-shigemori <159226636+m-shigemori@users.noreply.github.com>
2025-07-07 22:40:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
-
 echo "╔══╣ Install: YOLO ROS (STARTING) ╠══╗"
 
 cd ..
 git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_msgs.git
-cd sobits_msgs/
-bash install.sh
-cd ..
 git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/bbox_to_tf.git
 cd bbox_to_tf/
 bash install.sh
@@ -14,6 +10,9 @@ cd ..
 pip3 install torch
 pip3 install typing-extensions
 pip3 install ultralytics
+pip3 install numpy==1.24.2
 pip3 install lap
+
+sudo apt install ros-$ROS_DISTRO-vision-msgs
 
 echo "╚══╣ Install: YOLO ROS (FINISHED) ╠══╝"
