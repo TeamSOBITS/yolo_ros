28a74307366706577f288b167209330f1dc84802
apparettedare <ksuzuki9541@gmail.com>
2025-11-01 02:40:11 +0900

    [UPD] pip3 --break-system-packages

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ cd bbox_to_tf/
 bash install.sh
 cd ..
 
-pip3 install torch
-pip3 install typing-extensions
-pip3 install ultralytics
-pip3 install numpy==1.24.2
-pip3 install lap
+pip3 install torch --break-system-packages
+pip3 install typing-extensions --break-system-packages
+pip3 install ultralytics --break-system-packages
+pip3 install numpy==1.24.2 --break-system-packages
+pip3 install lap --break-system-packages
 
 sudo apt install ros-$ROS_DISTRO-vision-msgs
 
