039becb80c6c5d3c0371049c12f87f8fd60b025a
OnoFumiya <fumiyaono.choi@gmail.com>
2025-12-15 13:41:08 +0900

    Fix the path of install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ echo "╔══╣ Install: YOLO ROS (STARTING) ╠══╗"
 
 cd ..
 git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_interfaces.git
-git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/bbox_to_tf.git
-cd bbox_to_tf/
+git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/image_to_position.git
+cd image_to_position/
 bash install.sh
 cd ..
 
