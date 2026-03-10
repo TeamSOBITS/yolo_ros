c5ae963840928bb74113bba227c7641bc8377ff3
MrKeith99 <kvalentincardenas@hotmail.com>
2026-03-10 23:30:36 +0900

    [FIX] do not fix numpy model

--- install.sh
+++ install.sh
@@ -1,18 +1,41 @@
 echo "╔══╣ Install: YOLO ROS (STARTING) ╠══╗"
 
-cd ..
-git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_interfaces.git
-git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/image_to_position.git
-cd image_to_position/
-bash install.sh
+
+# Keep track of the current directory
+DIR=`pwd`
 cd ..
 
-pip3 install torch --break-system-packages
-pip3 install typing-extensions --break-system-packages
-pip3 install ultralytics --break-system-packages
-pip3 install numpy==1.24.2 --break-system-packages
-pip3 install lap --break-system-packages
+# Download required packages
+ros_packages=(
+    "sobits_interfaces"
+    "image_to_position"
+)
 
-sudo apt install ros-$ROS_DISTRO-vision-msgs
+#Clone all packages
+for ((i = 0; i < ${#ros_packages[@]}; i++)) {
+    echo "Clonning: ${ros_packages[i]}"
+    git clone --recurse-submodules -b $ROS_DISTRO-devel https://github.com/TeamSOBITS/${ros_packages[i]}.git
+
+    # Check if install.sh exists in each package
+    if [ -f ${ros_packages[i]}/install.sh ]; then
+        echo "Running install.sh in ${ros_packages[i]}."
+        cd ${ros_packages[i]}
+        bash install.sh
+        cd ..
+    fi
+}
+
+# Go back to previous directory
+cd ${DIR}
+
+python3 -m pip install torch --break-system-packages
+python3 -m pip install typing-extensions --break-system-packages
+python3 -m pip install ultralytics --break-system-packages
+python3 -m pip install 'numpy<2' --break-system-packages
+python3 -m pip install lap --break-system-packages
+
+sudo apt install -y \
+    ros-$ROS_DISTRO-vision-msgs
+
 
 echo "╚══╣ Install: YOLO ROS (FINISHED) ╠══╝"
